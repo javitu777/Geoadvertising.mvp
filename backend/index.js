a8ef1a9e40ccebb967f39c08dc6ef6a77886307d
javitu777 <javiturbe787@gmail.com>
2025-07-08 11:48:04 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
  const express = require("express");
 const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
-const bodyParser = require("body-parser");
-
+app.use(bodyParser.json());
 const app = express();
 const port = process.env.PORT || 10000;
 
